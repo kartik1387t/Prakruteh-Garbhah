8ec55616a86ff05b4966dda2dc74d857cd2b003f
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-02-12 23:51:36 +0530

    Refactor root rendering in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,13 +1,14 @@
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-try {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
