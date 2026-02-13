d562f82fb05f0d0825b9cde17d97db3009b5dd66
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-02-13 14:43:03 +0530

    Update csv.service.ts

--- src/services/csv.service.ts
+++ src/services/csv.service.ts
@@ -1,12 +1,21 @@
-export const fetchCSVData = async () => {
-  const response = await fetch(
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7BOsfu5CDJunvbj_NTPaZOFT23j4SREv1fxHWgcT4Y0Z0ifElDCHwQbt7LN8Xd2jfW14KAt4GtCyT/pub?gid=0&single=true&output=csv"
-  );
+import Papa from "papaparse";
+
+const CSV_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7BOsfu5CDJunvbj_NTPaZOFT23j4SREv1fxHWgcT4Y0Z0ifElDCHwQbt7LN8Xd2jfW14KAt4GtCyT/pub?gid=0&single=true&output=csv";
+
+export const fetchMirrorData = async () => {
+  const response = await fetch(CSV_URL);
 
   if (!response.ok) {
-    throw new Error("Failed to fetch CSV");
+    throw new Error("Failed to fetch Mirror CSV");
   }
 
   const text = await response.text();
-  return text;
+
+  const parsed = Papa.parse(text, {
+    header: true,
+    skipEmptyLines: true,
+  });
+
+  return parsed.data;
 };
