e1217a4ba93960f433e71c6fbed7c06c30ba6226
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-02-13 14:31:13 +0530

    Update csv.service.ts

--- src/services/csv.service.ts
+++ src/services/csv.service.ts
@@ -3,6 +3,10 @@ export const fetchCSVData = async () => {
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7BOsfu5CDJunvbj_NTPaZOFT23j4SREv1fxHWgcT4Y0Z0ifElDCHwQbt7LN8Xd2jfW14KAt4GtCyT/pub?gid=0&single=true&output=csv"
   );
 
+  if (!response.ok) {
+    throw new Error("Failed to fetch CSV");
+  }
+
   const text = await response.text();
   return text;
 };
