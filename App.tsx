fd091d77c18fea388dfff3ceab5e7a7e914da4b4
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-02-28 14:04:28 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -427,7 +427,7 @@ const handleLogout = async () => {
                       user={userProfile} 
                       onLoginRequest={() => { closeSlide(); setShowAuthModal(true); }}
                       onLogout={handleLogout}
-                      onUpdateBudget={() => {}}
+                      onUpdateBudget={() => {}} />
                     <Footer />
                  </div>
              </div>
