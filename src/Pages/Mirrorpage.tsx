0c13276ac2fdc1e568b2adfd57d19c59a9fc320e
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-03-02 17:39:07 +0530

    Create Mirrorpage.tsx

---
+++ src/Pages/Mirrorpage.tsx
@@ -0,0 +1,9 @@
+import MirrorSearch from "../components/MirrorSearch";
+
+export default function MirrorPage() {
+  return (
+    <div className="min-h-screen pt-24">
+      <MirrorSearch />
+    </div>
+  );
+}
