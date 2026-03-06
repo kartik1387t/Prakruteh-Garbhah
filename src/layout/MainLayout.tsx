a7a5f96b3a7c5e1de75f07b8dbd86ba8e1f6a94d
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-03-06 13:53:14 +0530

    Update MainLayout.tsx

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,10 @@
 import CosmicBackground from "../components/CosmicBackground";
 import SmartSearchBar from "../components/SmartSearchBar";
 import UniversalNavOrb from "../components/UniversalNavOrb";
+import SwarRangPlayer from "../components/SwarRangPlayer";
+import YatraPlanner from "../components/YatraPlanner";
+import SafetyDashboard from "../components/SafetyDashboard";
+import BharatAIGuide from "../components/BharatAIGuide";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
@@ -15,6 +19,12 @@ export default function MainLayout() {
 
       {/* Orb Navigation */}
       <UniversalNavOrb />
+
+      {/* Global Platform Systems */}
+      <YatraPlanner />
+      <SafetyDashboard />
+      <BharatAIGuide />
+      <SwarRangPlayer />
 
       {/* Page Content */}
       <main className="relative z-10">
