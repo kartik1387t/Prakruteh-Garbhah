daaeff8e040af9acc7b13fd6efcd03640ee308bf
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-03-06 16:49:16 +0530

    Update CosmicBackground.tsx

--- src/components/CosmicBackground.tsx
+++ src/components/CosmicBackground.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const CosmicBackground: React.FC = () => {
   const [offsetY, setOffsetY] = useState(0);
