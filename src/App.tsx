cd28936afc7ae03fed4bced31dafde6ccb7005fd
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-03-06 13:35:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainLayout from "./layout/MainLayout";
 
 import HomePage from "./pages/HomePage";
 import MirrorPage from "./pages/MirrorPage";
 import MapPage from "./pages/MapPage";
 import DashboardPage from "./pages/DashboardPage";
 
-const App: React.FC = () => {
+export default function App() {
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/mirror" element={<MirrorPage />} />
-        <Route path="/map" element={<MapPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+
+        <Route element={<MainLayout />}>
+
+          <Route path="/" element={<HomePage />} />
+          <Route path="/mirror" element={<MirrorPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+
+        </Route>
+
       </Routes>
+
     </BrowserRouter>
   );
-};
-
-export default App;
+}
