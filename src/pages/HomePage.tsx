fd11cec12e7cac434881ca9cc82d67d1644d9166
kartik1387t <252034687+kartik1387t@users.noreply.github.com>
2026-03-10 16:52:06 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import MirrorSearch from "../components/MirrorSearch";
 
 export default function HomePage() {
   return (
-    <div className="relative z-20">
+    <div className="relative w-full min-h-screen">
       <MirrorSearch />
     </div>
   );
